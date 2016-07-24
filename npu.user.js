d407b0068baeb8b767244c4e260560acca9c7347
Mate Solymosi <mate@solymo.si>
2016-07-24 03:32:15 +0200

    Bump version

--- npu.user.js
+++ npu.user.js
@@ -2,7 +2,7 @@
 // @name           Neptun PowerUp!
 // @namespace      http://example.org
 // @description    Felturbózza a Neptun-odat
-// @version        1.49.8
+// @version        1.49.9
 // @include        https://*neptun*/*hallgato*/*
 // @include        https://*hallgato*.*neptun*/*
 // @include        https://netw*.nnet.sze.hu/hallgato/*

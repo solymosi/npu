b6ca6dde4b716309aaa0cddbad940ee2aa55664e
Daniel Horvath <hodani95@me.com>
2020-08-31 09:18:51 +0200

    Megváltozott a corvinus neptunjának url-je

--- npu.user.js
+++ npu.user.js
@@ -5,6 +5,7 @@
 // @version        1.52.9
 // @downloadURL    https://github.com/solymosi/npu/raw/master/npu.user.js
 // @include        https://*neptun*/*hallgato*/*
+// @include        https://*neptun*/*Hallgatoi*/*
 // @include        https://*neptun*/*oktato*/*
 // @include        https://*hallgato*.*neptun*/*
 // @include        https://*oktato*.*neptun*/*

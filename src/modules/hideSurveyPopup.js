ebc1098ca45fc6188a359d8b94bc1354a96ff1b9
nyuszika7h <nyuszika7h@gmail.com>
2021-01-26 16:59:42 +0100

    Update comment style

--- src/modules/hideSurveyPopup.js
+++ src/modules/hideSurveyPopup.js
@@ -1,7 +1,7 @@
 const $ = window.jQuery;
 const utils = require("../utils");
 
-/* Hide popup notifying the user about unfilled surveys on every login */
+// Hide popup notifying the user about unfilled surveys on every login
 function hideSurveyPopup() {
   $(".ui-dialog:has(#startupPopup_lblOpinion) .ui-dialog-titlebar-close").click();
 }

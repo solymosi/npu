90d6614838adeb8d9f0f6216ee9aa9bfa8b982d6
FZs <zsombor0305fzs@gmail.com>
2023-08-28 14:21:48 +0200

    Fix stuff and formatting in modalClose.js

--- src/modules/modalClose.js
+++ src/modules/modalClose.js
@@ -1,17 +1,17 @@
 const utils = require("../utils");
 
 function addEvent() {
-  window.addEventListener('click', () => {
+  window.addEventListener("click", event => {
     // If the click is outside an open modal
-    if(event.target.matches('.ui-widget-overlay')) {
-      const closeButtons = document.querySelectorAll('.ui-dialog-titlebar-close');
-      const lastCloseButton = closeButtons[closeButtons.length-1];
-      if(lastCloseButton) {
+    if (event.target.matches(".ui-widget-overlay")) {
+      const closeButtons = document.querySelectorAll(".ui-dialog-titlebar-close");
+      const lastCloseButton = closeButtons[closeButtons.length - 1];
+      if (lastCloseButton) {
         //Close the topmost modal
         lastCloseButton.click();
       }
     }
-  })
+  });
 }
 module.exports = {
   shouldActivate: () => utils.isLoggedIn(),

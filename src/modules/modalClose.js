007089947df18348157a1c82107b15b5f92ec963
FZs <zsombor0305fzs@gmail.com>
2023-08-28 15:06:55 +0200

    Fix for modals that have no close button

--- src/modules/modalClose.js
+++ src/modules/modalClose.js
@@ -3,19 +3,17 @@ const utils = require("../utils");
 function addEvent() {
   window.addEventListener("click", event => {
     // If the click is outside an open modal
-    if (event.target.matches(".ui-widget-overlay")) {
-      const closeButtons = document.querySelectorAll(".ui-dialog-titlebar-close");
-      const lastCloseButton = closeButtons[closeButtons.length - 1];
-      if (lastCloseButton) {
-        //Close the topmost modal
-        lastCloseButton.click();
+    if (event.target.classList.contains("ui-widget-overlay")) {
+      const dialogs = document.querySelectorAll(".ui-dialog");
+      const topmostDialog = dialogs[dialogs.length - 1];
+      const closeButton = topmostDialog.querySelector(".ui-dialog-titlebar-close");
+      if (closeButton) {
+        closeButton.click();
       }
     }
   });
 }
 module.exports = {
   shouldActivate: () => utils.isLoggedIn(),
-  initialize: () => {
-    addEvent();
-  },
+  initialize: addEvent,
 };
